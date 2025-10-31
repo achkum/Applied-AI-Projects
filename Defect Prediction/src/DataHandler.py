e7027781886d9393a7e3c7cc403c6b76f6e8936a
Achyuth Kumar Baddela <achbad-5@student.ltu.se>
2025-10-31 22:41:22 +0100

    First Code Commit to this repo

---
+++ Defect Prediction/src/DataHandler.py
@@ -0,0 +1,59 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class DataHandler:
+    def __init__(self,data_path):
+        self.data_path = data_path
+        self.df = None
+        self.X_train = self.X_test = self.y_train = self.y_test = None
+        self.scaler = StandardScaler()
+    
+    def read_data(self):
+        """Read the data based on the path given"""
+        try:
+            self.df = pd.read_csv(self.data_path)
+        except:
+            print("Something went wrong while reading CSV. Please check path again!")
+        
+        print("Data Loaded successfully")
+        print(f"Shape :{self.df.shape}")
+        return self.df
+    
+    def explore_data(self):
+         """Display dataset overview."""
+         print("\n--- Dataset Overview ---")
+         print(f"\nFirst 20 rows :{self.df.head(20)}")
+         print(self.df.info)
+         print("\n Missing Values \n",self.df.isnull().sum())
+         print("\n Class distribution \n")
+         print(self.df['Defective'].value_counts(normalize=True))
+         sns.countplot(x='Defective', data=self.df)
+         plt.title("Defect Distribution")
+         plt.show()
+    
+    def preprocess(self):
+        """Clean the data and then split into train and test sets"""
+        df =  self.df.dropna()
+        df['Defect'] = df['Defective'].map({'Y': 1,'N': 0}) # Creating new label with values 0 and 1
+        X = df.drop(['Defective','Defect'],axis = 1) # All the columns/Features except Classifier
+        y = df['Defect']  #The Classification we need to predict
+
+        X_scaled = self.scaler.fit_transform(X)  #Scaling so that the data is normalized
+
+        #Splitting the Data
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X_scaled, y, test_size=0.2, random_state=42, stratify=y
+        )
+
+        print(f"\nData Split completed :\n {self.X_train.shape[0]} train,{self.X_test.shape[0]} test samples")
+        return self.X_train, self.X_test, self.y_train, self.y_test
+    
+
+
+
+
+
+
---
+++ Defect Prediction/src/ModelManager.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+class ModelManager:
+    def __init__(self):
+        self.models = {
+            "Logistic Regression" : LogisticRegression(max_iter = 1000),
+            "Random Forest" : RandomForestClassifier(n_estimators = 100, random_state = 42),
+            "XGBoost" : XGBClassifier(use_label_encoder = False, eval_metric = 'logloss', random_state = 42)
+        }
+        self.results = pd.DataFrame()
+
+    def train_all(self,X_train,y_train):
+        """Training each model"""
+        print("Training the models")
+        for name, model in self.models.items():
+            print("Training Model:",name)
+            model.fit(X_train,y_train)
+        print("All models trained successfully")
+    
+    def predict_all(self,X_test):
+        """Checking Predictions for all each"""
+        predictions = {}
+        for name,model in self.models.item():
+            predictions[name] = model.predict(X_test)
+        return predictions
+    
+    
+    
