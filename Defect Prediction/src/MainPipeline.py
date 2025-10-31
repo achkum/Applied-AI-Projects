f1b9ba664760275d735cb667c02dafc95c228cb9
Achyuth Kumar Baddela <achbad-5@student.ltu.se>
2025-10-31 22:41:22 +0100

    First Code Commit to this repo

---
+++ Defect Prediction/src/MainPipeline.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from DataHandler import DataHandler
+from ModelManager import ModelManager
+
+class DefectPredictionPipeline:
+    """Main pipeline orchestrating the workflow."""
+    def __init__(self,data_path):
+        self.data_handler = DataHandler(data_path)
+        self.model_manager = ModelManager()
+
+    def run(self):
+        # Call Data Handler to read, explore, preprocess data, and split data
+        self.data_handler.read_data()
+        self.data_handler.explore_data()
+        X_train, X_test, y_train, y_test = self.data_handler.preprocess()
+
+        # Call Model manager to initialize Models and train models
+        self.model_manager.train_all(X_train,y_train)
+        #Call Visualize the data before feeding to model
+        #Call Evaluator to evaluate performance
+        #Call Visualizer to Visualize again.
+        return None
+    
+
+if __name__ == "__main__":
+    data_path = str(Path(__file__).resolve().parent.parent / "data" / "defect_data.csv")
+    pipeline = DefectPredictionPipeline(data_path)
+    pipeline.run()
